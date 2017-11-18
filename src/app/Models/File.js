82d1ab932ad3e72a75583744c14fed29c4854b4b
Anthony Scinocco <scinocco.a@gmail.com>
2017-11-18 18:46:40 -0500

    added relationships for file to users

--- src/app/Models/File.js
+++ src/app/Models/File.js
@@ -3,6 +3,13 @@
 const Model = use('Model')
 
 class File extends Model {
+  owner () {
+    return this.hasOne('App/Models/User')
+  }
+
+  viewers () {
+    return this.hasMany('App/Models/User')
+  }
 }
 
 module.exports = File
