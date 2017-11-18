7d0ac3d5ffba1dab4a229b6008cec1e60783d346
Anthony Scinocco <scinocco.a@gmail.com>
2017-11-18 17:54:50 -0500

    added eslint, setup factories and seeders for users, updated user migration, to have some additional fields

--- src/database/factory.js
+++ src/database/factory.js
@@ -11,12 +11,16 @@
 |
 */
 
-// const Factory = use('Factory')
+const Factory = use('Factory')
+const Hash = use('Hash')
 
-/**
-  Factory.blueprint('App/Models/User', (faker) => {
-    return {
-      username: faker.username()
-    }
-  })
-*/
+Factory.blueprint('App/Models/User', async (faker) => {
+  return {
+    firstName: faker.first(),
+    middleName: faker.first(),
+    lastName: faker.last(),
+    email: faker.email(),
+    password: await Hash.make(faker.password())
+  }
+})
+
--- src/database/migrations/1503250034279_user.js
+++ src/database/migrations/1503250034279_user.js
@@ -6,7 +6,9 @@ class UserSchema extends Schema {
   up () {
     this.create('users', table => {
       table.increments()
-      table.string('username', 80).notNullable().unique()
+      table.string('firstName', 80).notNullable()
+      table.string('lastName', 80).notNullable()
+      table.string('middleName', 80).notNullable()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
       table.timestamps()
