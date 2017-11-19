4b87e89fb1942519de6ad5b07ecd55866e797a1e
Anthony Scinocco <scinocco.a@gmail.com>
2017-11-18 19:01:15 -0500

    added file model migration

---
+++ src/database/migrations/1511048957337_files_schema.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class FilesSchema extends Schema {
+  up () {
+    this.create('files', (table) => {
+      table.increments()
+      table.string('name', 254).notNullable().unique()
+      table.string('type', 80).notNullable() // file or folder
+      table.string('extension', 20).notNullable()
+      table.boolean('favorite').notNullable().default(false)
+      table.boolean('pinned').notNullable().default(false)
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('files')
+  }
+}
+
+module.exports = FilesSchema
